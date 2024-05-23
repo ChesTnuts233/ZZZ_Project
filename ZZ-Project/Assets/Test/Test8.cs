f4ba149839f0d5b09ccbe5a42570b03b973c48b1
ChesTnutsKoo <90549271+ChesTnuts233@users.noreply.github.com>
2024-05-23 14:27:19 +0800

    同步

--- ZZ-Project/Assets/Test/Test8.cs
+++ ZZ-Project/Assets/Test/Test8.cs
@@ -15,7 +15,6 @@ namespace GameBuild
 		public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
 		{
 			base.OnStateEnter(animator, stateInfo, layerIndex);
-
 		}
 	}
 }
